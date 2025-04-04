a1cbe50cd994578e63ed2277b53c2e841855c659
Shakim Harris <amenraprod@gmail.com>
2025-04-04 14:34:37 -0400

    setup routing

--- discord-frontend/src/main.tsx
+++ discord-frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useNavigate, Routes } from 'react-router-dom' // Import useNavigate for routing
 import './index.css'
 import '@mantine/core/styles.css';
 import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn, RedirectToSignUp } from '@clerk/clerk-react' // Ensure you have the correct import path
 import { MantineProvider } from '@mantine/core'
 import App from './App.tsx'
+
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   <>
@@ -13,12 +15,25 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     <RedirectToSignIn />
       </SignedOut></>
 }
-
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
+}
+
+
+const RouterComponent = () => {
+  const navigate = useNavigate();
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}
+     navigate={(to)=>navigate(to)}>
+      <Routes>
+
+      </Routes>
+
+     </ClerkProvider>
+  )
 }
 
 createRoot(document.getElementById('root')!).render(
