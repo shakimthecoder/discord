77197c0d15bce53207b680f25c20abe67daea19b
Shakim Harris <amenraprod@gmail.com>
2025-04-04 15:28:55 -0400

    add sidebar to routing

--- discord-frontend/src/layout/RootLayout.tsx
+++ discord-frontend/src/layout/RootLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from '../components/navigation/Sidebar' // Ensure the path is correct
 
 function RouteLayout() {
   return (
     <div>
+      <Sidebar />
       <Outlet />
     </div>
   )
