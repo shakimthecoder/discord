a239275b7851e51032a704d82df96be8f5552812
Shakim Harris <amenraprod@gmail.com>
2025-04-04 14:24:13 -0400

    add route

--- discord-frontend/src/main.tsx
+++ discord-frontend/src/main.tsx
@@ -2,13 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@mantine/core/styles.css';
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn, RedirectToSignUp } from '@clerk/clerk-react' // Ensure you have the correct import path
 import { MantineProvider } from '@mantine/core'
 import App from './App.tsx'
 
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  <>
+  <SignedIn>{children}</SignedIn>
+  <SignedOut>
+    <RedirectToSignIn />
+      </SignedOut></>
+}
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+     <MantineProvider>
+      <App />
     </MantineProvider>
+    </ClerkProvider>
   </StrictMode>,
 )
