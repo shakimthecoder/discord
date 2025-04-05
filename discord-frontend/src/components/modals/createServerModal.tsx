d44cb897d75c3d2156cab959e4adb8650d593b42
Shakim Harris <amenraprod@gmail.com>
2025-04-05 19:14:55 -0400

    add form

--- discord-frontend/src/components/modals/createServerModal.tsx
+++ discord-frontend/src/components/modals/createServerModal.tsx
@@ -1,9 +1,31 @@
-import { Modal } from '@mantine/core';
+import { Modal, Stack, Flex } from '@mantine/core';
 import { useModal } from '../../../hooks/useModal';
+import { useForm } from '@mantine/form';
+import { Text } from '@mantine/core';
 
 export function CreateServerModal() {
     const { isOpen, closeModal } = useModal("CreateServer");
+    const form = useForm({
+        initialValues: {
+            name: '',
+        },
+        validate: {
+            name: (value) => !value.trim && 'Please enter a name for your server',
+        }
+    })
     return (
-        <Modal opened={isOpen} onClose={closeModal}>Create New Server</Modal>
+        <Modal title="Create a server" opened={isOpen} 
+         onClose={closeModal}>Create New Server
+          <Text c="dimmed">
+            Give your server a personality. You can change this later.
+          </Text>
+          <form onSubmit={() => {}}>
+            <Stack >
+                <Flex justify="center" align="center" direction="column">
+                </Flex>
+            </Stack>
+
+          </form>
+        </Modal>
     )
 }
