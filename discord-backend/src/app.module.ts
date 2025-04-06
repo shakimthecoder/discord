dfef35988a445b1f250cab6a8d24119e3b65284d
Shakim Harris <amenraprod@gmail.com>
2025-04-06 00:59:52 -0400

    add apollo driver

--- discord-backend/src/app.module.ts
+++ discord-backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    }),
+    GraphQLModule.forRootAsync({
+      imports: [],
+      inject: [],
+      driver: ApolloDriver,
+      useFactory: async () => {
+        return {
+          autoSchemaFile: join(process.cwd(), 'src', 'schema.gql'),
+          subscriptions: {}
+        }
+      }
+    })
+   ],
   controllers: [AppController],
   providers: [AppService],
 })
