9921f0e3b3c523f59c3575235d8dcaf608d8bdeb
Shakim Harris <amenraprod@gmail.com>
2025-04-08 01:28:45 -0400

    add auth guard to resolvers

--- discord-backend/src/profile/profile.resolver.ts
+++ discord-backend/src/profile/profile.resolver.ts
@@ -2,12 +2,14 @@ import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { CreateProfileDto } from './dto';
 import { ProfileService } from './profile.service';
 import { Profile } from './profile.types';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/authGuard';
 
 @Resolver()
 export class ProfileResolver {
     constructor(private readonly profileService: ProfileService){}
    
-
+@UseGuards(GraphqlAuthGuard)
 @Mutation(() => Profile)
 async createProfile(@Args('input') input: CreateProfileDto) {
     return this.profileService.createProfile(input);
