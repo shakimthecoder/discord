4e13a65ee968f78c9aa49653772f9ac043aab942
Shakim Harris <amenraprod@gmail.com>
2025-04-07 23:38:39 -0400

    create profile method on profile service

---
+++ discord-backend/src/profile/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src//prisma.service';
+import { CreateProfileDto } from './dto';
+
+@Injectable()
+export class ProfileService {
+    constructor(private readonly prisma: PrismaService){}
+
+async createProfile(createProfileDto: CreateProfileDto) {
+    const profile = await this.prisma.profile.findUnique({
+        where: {
+            email: createProfileDto.email,
+        }
+    });
+    if(profile) {
+        return profile;
+    }
+    return this.prisma.profile.create({
+        data: createProfileDto,
+    })
+
+}
+}
