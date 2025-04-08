14d836aa9322c4efbcf7f4fb15237f7443123e9a
Shakim Harris <amenraprod@gmail.com>
2025-04-08 00:20:43 -0400

    add mutation for profile

--- discord-backend/src/profile/profile.resolver.ts
+++ discord-backend/src/profile/profile.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { CreateProfileDto } from './dto';
+import { ProfileService } from './profile.service';
+import { Profile } from './profile.types';
 
 @Resolver()
-export class ProfileResolver {}
+export class ProfileResolver {
+    constructor(private readonly profileService: ProfileService){}
+   
+
+@Mutation(() => Profile)
+async createProfile(@Args('input') input: CreateProfileDto) {
+    return this.profileService.createProfile(input);
+}
+@Mutation(() => Profile)
+async getProfiledById(@Args('id') profileId: number) {
+    return this.profileService.getProfileById(profileId);
+}
+
+}
