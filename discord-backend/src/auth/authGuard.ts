4ba29d9aa474362b1c4bbe03e8c0dcdb60d20fca
Shakim Harris <amenraprod@gmail.com>
2025-04-08 01:29:13 -0400

    catch payload jwt token

--- discord-backend/src/auth/authGuard.ts
+++ discord-backend/src/auth/authGuard.ts
@@ -3,14 +3,28 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { JwtService } from '@nestjs/jwt';
 
 export class GraphqlAuthGuard implements CanActivate {
-    constructor(private jwtService: JwtService) {
-        async CanActivate(context: GqlExecutionContext){
+    constructor(private jwtService: JwtService) {} 
+        async canActivate(context: GqlExecutionContext) {
             const gqlCtx = context.getArgByIndex(2);
             const request: Request = gqlCtx.req;
             const token = this.extractToken(request);
             if(!token) {
                 throw new UnauthorizedException;
             }
+            try {
+                const payload = await this.jwtService.verifyAsync(token, {
+                    publicKey: process.env.JWT_PUBLIC_KEY,
+                    algorithms: ['RS256'],
+                });
+                request['Profile'] = payload;
+
+            } catch (error) {
+                throw new UnauthorizedException('Not authorized');
+
+            }
+            return true;
+        }
+        private extractToken(request: Request): string | undefined  {
+            return request.headers.authorization?.replace('Bearer', '');
         }
     }
-}
