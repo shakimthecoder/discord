6ea7ae7fb0b1c3c190563f3a0fe42e1e94f943a3
Shakim Harris <amenraprod@gmail.com>
2025-04-10 16:07:49 -0400

    request error handling

--- discord-backend/src/server/server.resolver.ts
+++ discord-backend/src/server/server.resolver.ts
@@ -1,12 +1,18 @@
 import { Resolver, Query } from '@nestjs/graphql';
 import { Server } from './types'
-import { Args } from '@nestjs/graphql';
+import { Args, Context } from '@nestjs/graphql';
+import { ApolloError } from 'apollo-server-express';
 
 @Resolver()
 export class ServerResolver {
     @Query(() => [Server])
     async getServers(
       @Args('profileId') profileId: number,
-    ){}
+      @Context() ctx: { req: Request }
+    ){
+      if(!ctx.req?.profile.email){
+         return new ApolloError('Profile not found', 'PROFILE_NOT_FOUND');
+      }
+    }
     
 }
